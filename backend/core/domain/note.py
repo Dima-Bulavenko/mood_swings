abc9bafba9123f8bb0934cefd666d204554ce4c5
Dima Bulavenko <dima99bylovenko@gmail.com>
2026-02-26 22:28:16 +0000

    fix: add missing id field to Note model

--- backend/core/domain/note.py
+++ backend/core/domain/note.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class Note(BaseModel):
+    id: str
     user_id: str
     note: str = Field(max_length=100)
     date_create: date = Field(default_factory=date.today)
